e3ac58ad57ca38e363f210419f6e6d4f1fc0d7f0
Carlos Alberto <carlosavjr513@gmail.com>
2024-06-04 10:31:43 -0300

    MontlyResume component update

---
+++ src/app/components/MontlyResume/MontlyResume.tsx
@@ -0,0 +1,103 @@
+import { Grid, Typography } from "@mui/material";
+import React from "react";
+import MeetNewClientCard from "./MeetNewClientCard";
+import MontlyResumeCard from "./MontlyResumeCard";
+
+interface MonthlyResumeProps {
+  date: string;
+  running: [number, number];
+  scripting: number;
+  audience: any;
+}
+
+const MontlyResume: React.FC<MonthlyResumeProps> = ({
+  date,
+  running,
+  scripting,
+  audience,
+}) => {
+  return (
+    <Grid
+      container
+      xs={12}
+      sm={12}
+      md={12}
+      xl={12}
+      sx={{
+        display: "flex",
+        justifyContent: "space-evenly",
+        pt: 1,
+        backgroundColor: "#000000",
+        color: "#ffffff",
+        width: "100%",
+      }}
+    >
+      <Grid
+        item
+        xl={8}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+        }}
+      >
+        <Grid
+          item
+          sx={{
+            textAlign: "left",
+            px: 5,
+          }}
+        >
+          <Typography sx={{ fontSize: 10, fontWeight: 400 }}>
+            RESUMO MENSAL
+          </Typography>
+          <Typography variant="h5" sx={{ fontSize: 22, fontWeight: 700 }}>
+            {date}
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          sx={{
+            display: "flex",
+            px: 5,
+          }}
+        >
+          <Grid
+            container
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <Grid item xs={6} sm={6} md={4} xl={4}>
+              <MontlyResumeCard dataType="running" data={running} />
+            </Grid>
+            <Grid item xs={6} sm={6} md={4} xl={4}>
+              <MontlyResumeCard dataType="scripting" data={scripting} />
+            </Grid>
+            <Grid item xs={12} sm={12} md={4} xl={4}>
+              <MontlyResumeCard dataType="audience" data={audience} />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+
+      <Grid
+        item
+        xs={12}
+        sm={12}
+        md={3}
+        xl={3}
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <MeetNewClientCard />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default MontlyResume;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
 "use client";
-import { Add } from "@mui/icons-material";
-// prettier-ignore
-import { Button, Grid, Typography } from "@mui/material";
+import { Grid } from "@mui/material";
 import axios from "axios";
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import ContactsCard from "./components/Audience/ContactsCard";
 import SendedBalanceCard from "./components/Audience/SendedBalanceCard";
 import DashboardCredit from "./components/DashboardCredit/DashboardCredit";
-import MontlyResumeCard from "./components/MontlyResume/MontlyResumeCard";
+import MontlyResume from "./components/MontlyResume/MontlyResume";
 import MyResearchesCarousel from "./components/MyResearches/MyResearchesCarousel";
 import NotificationTabs from "./components/Notification/NotificationTabs";
 import { formatDate, splitRunning } from "./utils/functions";
-import MeetNewClientCard from "./components/MontlyResume/MeetNewClient";
 
 interface MyResearch {
   name: string;
@@ -72,106 +69,28 @@ export default function Home() {
   const { date, running, scripting, audience, credits, myResearches } = watch();
 
   return (
-    // GRID MÃE
     <Grid container sx={{ display: "flex" }}>
       {/* GRID IRMA DA FUCHSIA */}
       <Grid
         container
+        item
         xs={12}
-        sm={12}
-        md={12}
+        // sm={12}
+        // md={12}
         xl={9}
         sx={{
           display: "flex",
-          // backgroundColor: "#000000"
-          width: "100vw",
+          // backgroundColor: "#000000",
+          width: "100%",
         }}
       >
         {/* GRID INTERNA PRETA - RESUMO */}
-        <Grid
-          container
-          xs={12}
-          sm={12}
-          md={12}
-          xl={12}
-          sx={{
-            display: "flex",
-            justifyContent: "space-evenly",
-            pt: 1,
-            backgroundColor: "#000000",
-            color: "#ffffff",
-            width: "100vw",
-          }}
-        >
-          <Grid
-            item
-            xl={8}
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              gap: 2,
-            }}
-          >
-            <Grid
-              item
-              sx={{
-                textAlign: "left",
-                px: 5,
-              }}
-            >
-              <Typography
-                variant="body1"
-                sx={{ fontSize: 10, fontWeight: 400 }}
-              >
-                RESUMO MENSAL
-              </Typography>
-              <Typography variant="h5" sx={{ fontSize: 22, fontWeight: 700 }}>
-                {date}
-              </Typography>
-            </Grid>
-            <Grid
-              item
-              sx={{
-                display: "flex",
-                px: 5,
-              }}
-            >
-              <Grid
-                container
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-              >
-                <Grid item xs={6} sm={6} md={4} xl={4}>
-                  <MontlyResumeCard dataType="running" data={running} />
-                </Grid>
-                <Grid item xs={6} sm={6} md={4} xl={4}>
-                  <MontlyResumeCard dataType="scripting" data={scripting} />
-                </Grid>
-                <Grid item xs={12} sm={12} md={4} xl={4}>
-                  <MontlyResumeCard dataType="audience" data={audience} />
-                </Grid>
-              </Grid>
-            </Grid>
-          </Grid>
-
-          {/* NOVA PESQUISA CARTAO ROXO */}
-          <Grid
-            item
-            xs={12}
-            sm={12}
-            md={3}
-            xl={3}
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <MeetNewClientCard />
-          </Grid>
-        </Grid>
+        <MontlyResume
+          date={date}
+          running={running}
+          scripting={scripting}
+          audience={audience}
+        />
 
         {/* GRID AZUL */}
         <Grid
@@ -179,7 +98,6 @@ export default function Home() {
           md={9}
           xl={12}
           sx={{
-            backgroundColor: "#eef2f3",
             // backgroundColor: "#8000ff", // PLACEHOLDER
             px: 0,
             display: "flex",
@@ -215,6 +133,7 @@ export default function Home() {
             </Grid>
             <Grid
               container
+              item
               sm={10}
               md={5}
               xl={5}
@@ -235,10 +154,9 @@ export default function Home() {
           item
           md={3}
           sx={{
-            // backgroundColor: "#eef2f3",
             // backgroundColor: "#0ff",
             textAlign: "center",
-            display: { xs: "none", sm: "none", md: "block", xl: "none" },
+            display: { xs: "none", sm: "none", md: "flex", xl: "none" },
           }}
         >
           <NotificationTabs />
@@ -249,9 +167,8 @@ export default function Home() {
         item
         xl={3}
         sx={{
-          // backgroundColor: "#eef2f3",
           // backgroundColor: "#f0f",
-          display: { xs: "none", sm: "none", md: "none", xl: "block" },
+          display: { xs: "none", sm: "none", md: "none", xl: "flex" },
         }}
       >
         <NotificationTabs />
